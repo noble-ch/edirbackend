744943230b442ad66c96f26d1809d068137a7c14
noble biru <noblebarch@gmail.com>
2025-04-09 23:19:23 +0300

    memeber and edir creation

--- config/settings.py
+++ config/settings.py
@@ -57,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'tenants.middleware.EdirSlugMiddleware',
+     'whitenoise.middleware.WhiteNoiseMiddleware', 
 
 ]
 
@@ -130,6 +131,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
