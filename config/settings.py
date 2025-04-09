fad2ae1e21963d9eea897e4663e172ca613898dd
noble biru <noblebarch@gmail.com>
2025-04-09 20:17:43 +0300

    memeber and edir creation

--- config/settings.py
+++ config/settings.py
@@ -124,8 +124,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # Where collectstatic will put files
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',  # Your additional static directories
+]
+
+# For production, you should also add:
+if not DEBUG:
+    STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
